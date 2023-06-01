a159c0a127e4f9523b90464bd0a01e2d5e619321
azuki <syviet@outlook.com>
2023-06-01 11:25:12 +0200

    User Model

---
+++ Back-End/src/main/java/ch/web/web_shop/model/User.java
@@ -0,0 +1,82 @@
+package ch.web.web_shop.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private boolean subscribed;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(Long id, String name, String email, boolean subscribed, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.subscribed = subscribed;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isSubscribed() {
+        return subscribed;
+    }
+
+    public void setSubscribed(boolean subscribed) {
+        this.subscribed = subscribed;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", subscribed=" + subscribed +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
